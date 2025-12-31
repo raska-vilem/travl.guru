51765f4d4a47773b6b240cbad9956a8d87db4f4d
Vilém Raška <v.rashka.ext@make.com>
2025-12-31 11:57:50 +0100

    feat: init and add ui

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://prague.travl.guru',
+
+  vite: {
+      plugins: [tailwindcss()]
+  },
+
+  // integrations: [icon()]
+});
