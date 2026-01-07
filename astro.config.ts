109030148715821fd40f0200df2718d364c9361b
Vilém Raška <v.rashka.ext@make.com>
2026-01-07 17:37:37 +0100

    wip

--- astro.config.ts
+++ astro.config.ts
@@ -1,18 +1,17 @@
-import tailwindcss from "@tailwindcss/vite";
-import { defineConfig, envField, fontProviders } from "astro/config";
+import sitemap from '@astrojs/sitemap';
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig, envField, fontProviders } from 'astro/config';
+import icon from 'astro-icon';
+import { loadEnv } from 'vite';
 
-import sitemap from "@astrojs/sitemap";
-import icon from "astro-icon";
-import { loadEnv } from "vite";
-
-const { CDN_URL } = loadEnv("production", process.cwd(), "");
+const { CDN_URL } = loadEnv('production', process.cwd(), '');
 
 export default defineConfig({
-    site: "https://travl.guru",
+    site: 'https://travl.guru',
     env: {
         schema: {
-            CMS_URL: envField.string({ context: "server", optional: false, access: "public" }),
-            CDN_URL: envField.string({ context: "server", optional: false, access: "public" }),
+            CMS_URL: envField.string({ context: 'server', optional: false, access: 'public' }),
+            CDN_URL: envField.string({ context: 'server', optional: false, access: 'public' }),
         },
     },
     vite: {
@@ -20,14 +19,14 @@ export default defineConfig({
     },
     integrations: [sitemap(), icon()],
     prefetch: {
-        defaultStrategy: "viewport",
+        defaultStrategy: 'viewport',
     },
     experimental: {
         fonts: [
             {
                 provider: fontProviders.google(),
-                cssVariable: "--font-roboto",
-                name: "Roboto",
+                cssVariable: '--font-roboto',
+                name: 'Roboto',
             },
         ],
     },
